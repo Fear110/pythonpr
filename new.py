8b6f1c675e118e5e3948daa9e0855762c37cdfdc
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-06-17 10:28:38 +0545

    last we go

--- new.py
+++ new.py
@@ -1,3 +1,11 @@
+#variable is declared when you assign value to it
+x = 10
+print(x)
+
+a = "name"
+print(a)
+
+"""
 a = 0 ;
 while (a <= 5):
     print(a)
@@ -14,7 +22,4 @@ if a == 5:
     print("the result is 5")
 else:
     print("its not")
-
-
-
-
+"""
