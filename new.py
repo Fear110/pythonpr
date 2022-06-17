95ea5faa17d5181af0c9b2577f1e2918c76b747d
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-06-17 10:08:54 +0545

    here we go again

--- new.py
+++ new.py
@@ -1,14 +1,19 @@
-"""a = 0 ;
+a = 0 ;
 while (a <= 5):
     print(a)
     a=a+1
 else:
     print("final");
-"""
+
 animal = ["dog","cat","mouse"]
 for a in animal:
     print(a)
 
+a = 5
+if a == 5:
+    print("the result is 5")
+else:
+    print("its not")
 
 
 
